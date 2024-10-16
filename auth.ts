4443f9a2c68a990e4b2e25f470884da33ff72180
bc1phugo <hugo.oh.dev@gmail.com>
2024-10-16 21:21:51 +0900

    [setup] next auth twitter, discord provider done

--- auth.ts
+++ auth.ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
+import TwitterProvider from "next-auth/providers/twitter";
+import DiscordProvider from "next-auth/providers/discord";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [TwitterProvider, DiscordProvider],
 });

68e87657b80d64c0e07d0f0eb57955d88370f8de
bc1phugo <hugo.oh.dev@gmail.com>
2024-10-24 01:59:02 +0900

    [feat] leaderboard WIP-1

---
+++ app/leaderboard/_components/columns.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import { AvatarFallback, Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { TrophyIcon } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal } from "lucide-react";
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+
+export type UserRanker = {
+  username: string;
+  connectionCount: number;
+  imgUrl?: string;
+};
+
+export const columns: ColumnDef<UserRanker>[] = [
+  {
+    accessorKey: "ranking",
+    header: "Ranking",
+    cell: ({ row }) => (
+      <div className="flex gap-2 items-center">
+        {row.index === 0 && <TrophyIcon className="w-6 h-6 fill-yellow-500" />}
+        {row.index === 1 && <TrophyIcon className="w-6 h-6 fill-gray-300" />}
+        {row.index === 2 && <TrophyIcon className="w-6 h-6 fill-amber-700" />}
+        {row.index > 2 && <TrophyIcon className="w-6 h-6 fill-gray-500" />}
+        {row.index + 1}
+      </div>
+    ),
+  },
+  {
+    accessorKey: "username",
+    header: "Username",
+    cell: ({ row }) => {
+      return (
+        <div className="flex items-center gap-2">
+          <Avatar>
+            <AvatarImage
+              src={row.original.imgUrl}
+              alt={row.original.username}
+            />
+            <AvatarFallback>
+              {row.original.username.slice(0, 2).toUpperCase()}
+            </AvatarFallback>
+          </Avatar>
+          <div className="text-lg font-bold">{row.original.username}</div>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "connectionCount",
+    header: "Connections",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const userRanker = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Connections</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(userRanker.username)}
+            >
+              Copy user handle
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View User</DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <Button className="h-6" size="sm">
+                Ask to Connect
+              </Button>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -1,38 +1,152 @@
 import { Separator } from "@/components/ui/separator";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
 
 export default function ProfilePage() {
-  // const leaderboardData = [
-  //   {
-  //     name: "Paul C",
-  //     imageSrc: "/images/profile/paul.png",
-  //     firstConnectionScore: 100,
-  //     secondConnectionScore: 100,
-  //   },
-  //   {
-  //     name: "Ping Ping",
-  //     imageSrc: "/images/profile/PingPing.png",
-  //     firstConnectionScore: 90,
-  //     secondConnectionScore: 90,
-  //   },
-  //   {
-  //     name: "ThisIsFin",
-  //     imageSrc: "/images/profile/ThisIsFin.png",
-  //     firstConnectionScore: 80,
-  //     secondConnectionScore: 80,
-  //   },
-  //   {
-  //     name: "Ssick",
-  //     imageSrc: "/images/profile/Ssick.png",
-  //     firstConnectionScore: 70,
-  //     secondConnectionScore: 70,
-  //   },
-  // ];
+  const leaderboardData = [
+    {
+      username: "baeksu",
+      connectionCount: 24,
+      imgUrl: "/images/profile/baeksu.jpg",
+    },
+    {
+      username: "Claire",
+      connectionCount: 19,
+      imgUrl: "/images/profile/Claire.jpg",
+    },
+    {
+      username: "endgame",
+      connectionCount: 15,
+      imgUrl: "/images/profile/endgame.jpg",
+    },
+    {
+      username: "buja",
+      connectionCount: 14,
+      imgUrl: "/images/profile/buja.jpg",
+    },
+    {
+      username: "whitesocks",
+      connectionCount: 13,
+      imgUrl: "/images/profile/whitesocks.jpg",
+    },
+    {
+      username: "PaulC",
+      connectionCount: 12,
+      imgUrl: "/images/profile/PaulC.jpg",
+    },
+    {
+      username: "builnad",
+      connectionCount: 12,
+      imgUrl: "/images/profile/builnad.jpg",
+    },
+    {
+      username: "Ssick",
+      connectionCount: 11,
+      imgUrl: "/images/profile/Ssick.jpg",
+    },
+    {
+      username: "overcome",
+      connectionCount: 11,
+      imgUrl: "/images/profile/overcome.jpg",
+    },
+    {
+      username: "berzan",
+      connectionCount: 9,
+      imgUrl: "/images/profile/berzan.jpg",
+    },
+    {
+      username: "Jinie",
+      connectionCount: 9,
+      imgUrl: "/images/profile/Jinie.jpg",
+    },
+    { username: "HB", connectionCount: 8, imgUrl: "/images/profile/HB.jpg" },
+    { username: "PingPing", connectionCount: 8 },
+    { username: "mingming", connectionCount: 8 },
+    {
+      username: "Lavadong",
+      connectionCount: 7,
+      imgUrl: "/images/profile/Lavadong.jpg",
+    },
+    {
+      username: "boba",
+      connectionCount: 7,
+      imgUrl: "/images/profile/boba.jpg",
+    },
+    {
+      username: "PolyMoly",
+      connectionCount: 6,
+      imgUrl: "/images/profile/PolyMoly.jpg",
+    },
+    {
+      username: "ozzy",
+      connectionCount: 6,
+      imgUrl: "/images/profile/ozzy.jpg",
+    },
+    {
+      username: "chamdom",
+      connectionCount: 5,
+      imgUrl: "/images/profile/chamdom.jpg",
+    },
+    {
+      username: "baram7",
+      connectionCount: 4,
+      imgUrl: "/images/profile/baram7.jpg",
+    },
+    {
+      username: "BenjaNad",
+      connectionCount: 4,
+      imgUrl: "/images/profile/BenjaNad.jpg",
+    },
+    {
+      username: "Choonsik",
+      connectionCount: 3,
+      imgUrl: "/images/profile/Choonsik.jpg",
+    },
+    {
+      username: "ShimMoney",
+      connectionCount: 2,
+      imgUrl: "/images/profile/ShimMoney.jpg",
+    },
+    {
+      username: "juju5378",
+      connectionCount: 2,
+      imgUrl: "/images/profile/juju5378.jpg",
+    },
+    {
+      username: "Grimjow",
+      connectionCount: 2,
+      imgUrl: "/images/profile/Grimjow.jpg",
+    },
+    {
+      username: "Jeongnam",
+      connectionCount: 2,
+      imgUrl: "/images/profile/Jeongnam.jpg",
+    },
+    {
+      username: "Seungjae",
+      connectionCount: 1,
+      imgUrl: "/images/profile/Seungjae.jpg",
+    },
+    {
+      username: "This is Fin",
+      connectionCount: 1,
+      imgUrl: "/images/profile/This is Fin.jpg",
+    },
+    {
+      username: "bakba",
+      connectionCount: 1,
+      imgUrl: "/images/profile/bakba.jpg",
+    },
+  ];
 
   return (
     <>
       <section className="flex  max-w-2xl lg:max-w-[1024px] w-full flex-col gap-5 p-5 ">
         <h1>Leaderboard</h1>
         <Separator />
+        <div className="container mx-auto py-10">
+          <DataTable columns={columns} data={leaderboardData} />
+        </div>
       </section>
     </>
   );
